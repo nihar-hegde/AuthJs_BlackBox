08531ae578ea797e30f522ca483ad67f47f8ac37
Nihar Hegde <niharhegde163@gmail.com>
2024-01-04 21:36:31 +0530

    oAuth setup done

--- auth.config.ts
+++ auth.config.ts
@@ -5,9 +5,19 @@ import credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
--- auth.ts
+++ auth.ts
@@ -2,6 +2,10 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
+
+import { getUserByid } from "./data/user";
+import { UserRole } from "@prisma/client";
+
 export const {
   handlers: { GET, POST },
   auth,
@@ -9,6 +13,54 @@ export const {
   signOut,
 } = NextAuth({
   // spread auth config because this file will contain the prsimaAdapter and it doese not support edge
+
+  // NOTE: This event is to prepopulate the verifiedEmail field in db if user loggs in using oAuth.
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
+  callbacks: {
+    // NOTE: what this does is that if the user is logged in but not verified then it will not allow to login.
+
+    // async signIn({ user }) {
+    //   const existingUser = await getUserByid(user.id);
+    //   if (!existingUser || !existingUser.emailVerified) {
+    //     return false;
+    //   }
+    //   return true;
+    // },
+
+    async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      // NOTE: if you don't have token . sub then you are logged out. so just return the token here itself
+      if (!token.sub) return token;
+
+      const existingUser = await getUserByid(token.sub);
+      if (!existingUser) return token;
+
+      // assign the role to the token
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
