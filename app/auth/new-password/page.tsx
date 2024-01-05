e707e705241983567c51c00dc31698e45ee11c08
Nihar Hegde <niharhegde163@gmail.com>
2024-01-05 21:45:45 +0530

    reset password functionality done

---
+++ app/auth/new-password/page.tsx
@@ -0,0 +1,7 @@
+import { NewPasswordForm } from "@/components/auth/new-password-form";
+import React from "react";
+
+const NewPasswordPage = () => {
+  return <NewPasswordForm />;
+};
+export default NewPasswordPage;
