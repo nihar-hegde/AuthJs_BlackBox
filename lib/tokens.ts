98fbdd9d4df0f428c8b741d367d628369040d80f
Nihar Hegde <niharhegde163@gmail.com>
2024-01-06 17:15:55 +0530

    2FA verification token expiry time to 5 mins

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -8,7 +8,7 @@ import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
   // NOTE: token expires in one hour may make it shorter like 10 or 15 mins
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
 
   const exisitngToken = await getTwoFactorTokenByEmail(email);
 
